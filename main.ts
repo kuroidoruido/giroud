ec0d7cd6b0a04f13121199b3fadc41dc96264893
Anthony Pena <anthony.pena@outlook.fr>
2023-11-06 00:30:49 +0100

    pioche équipe sansposte

--- main.ts
+++ main.ts
@@ -1,6 +1,46 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showNumber(randint(1, 6))
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    if (postes.length > 0) {
+        teamIndex = randint(0, postes.length - 1)
+        choosenTeam = postes.removeAt(teamIndex)
+        basic.showNumber(choosenTeam)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
 })
+input.onButtonPressed(Button.A, function () {
+    if (teamCount > 4) {
+        teamCount = teamCount - 1
+        basic.showNumber(teamCount)
+        initTeams()
+    } else {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.InBackground)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (teamCount < 8) {
+        teamCount = teamCount + 1
+        initTeams()
+        basic.showNumber(teamCount)
+    } else {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.sad), music.PlaybackMode.InBackground)
+    }
+})
+function initTeams () {
+    postes = []
+    for (let teamIndex = 0; teamIndex <= teamCount - 1; teamIndex++) {
+        for (let index = 0; index < 5; index++) {
+            postes.push(teamIndex + 1)
+        }
+    }
+    control.waitMicros(1000)
+    basic.showIcon(IconNames.SmallSquare)
+}
+let choosenTeam = 0
+let teamIndex = 0
+let postes: number[] = []
+let teamCount = 0
+teamCount = 6
+initTeams()
 basic.forever(function () {
 	
 })
